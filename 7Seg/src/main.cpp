502f91bb5475c64411eca638fb109b39f67edae0
leonbl <leon.blazic@gmail.com>
2023-10-23 13:19:16 +0200

    deluje

--- 7Seg/src/main.cpp
+++ 7Seg/src/main.cpp
@@ -2,11 +2,15 @@
 #define SDI 8
 #define CLK 7
 #define LATCH 4
-#define CLK_DELAY (10)
+#define CLK_DELAY (1)
 
 void toggleCLK(void);
 void writeToDisp(uint8_t dispNum, uint8_t dispVal);
 void toggleLatchCLK(void);
+void display(void);
+
+uint8_t number[10] = {0x03,0x9f,0x25,0x0d,0x99,0x49,0x41,0x1f,0x01,0x19};
+uint8_t disp_num[4] = {1,3,5,7};
 
 void setup() {
   pinMode(SDI, OUTPUT);
@@ -15,22 +19,22 @@ void setup() {
 }
 
 void loop() {
-  writeToDisp(0xff,0x00);
-  delay(1000);
+  uint32_t cas = millis();
+  while((millis()-cas)<100){
+    display();
+  }
+  disp_num[0]++;
+  if(disp_num[0]>=9) disp_num[0]=0;
 }
 
 void toggleCLK(void){
   digitalWrite(CLK, LOW);
-  delay(CLK_DELAY);
   digitalWrite(CLK, HIGH);
-  delay(CLK_DELAY);
 }
 
 void toggleLatchCLK(void){
   digitalWrite(LATCH, LOW);
-  delay(CLK_DELAY);
   digitalWrite(LATCH, HIGH);
-  delay(CLK_DELAY);
 }
 
 void writeToDisp(uint8_t dispNum, uint8_t dispVal){
@@ -45,4 +49,11 @@ void writeToDisp(uint8_t dispNum, uint8_t dispVal){
     dispNum = dispNum >> 1;
   }
   toggleLatchCLK();
+}
+
+void display(void){
+  writeToDisp(0x80,number[disp_num[0]]);
+  writeToDisp(0x40,number[disp_num[1]]);
+  writeToDisp(0x20,number[disp_num[2]]);
+  writeToDisp(0x10,number[disp_num[3]]);
 }
