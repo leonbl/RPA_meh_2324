e473372b94a4b9e0fa11ea08bec457bbbe7b414e
leonbl <leon.blazic@gmail.com>
2023-10-26 15:09:02 +0200

    tipke

---
+++ Tipke/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#define tipka 2
+#define led 13
+int staroStanje = 0;
+
+void setup()
+{
+  pinMode(tipka, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  int stanje = digitalRead(tipka);
+  int dCnt = 0;
+  if (staroStanje == 1 && stanje == 0)
+  {
+    for (int n = 0; n < 5; n++)
+    {
+      delay(5);
+      if (stanje == 0)
+        dCnt++;
+    }
+    if (dCnt > 2)
+      digitalWrite(led, !digitalRead(led));
+  }
+  staroStanje = stanje;
+}
