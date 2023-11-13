0f3b162f8c6b5c55d14fd25458547ac345376bfb
leonbl <leon.blazic@gmail.com>
2023-11-13 11:58:47 +0100

    updated

--- regulacija_osvetlitve/src/main.cpp
+++ regulacija_osvetlitve/src/main.cpp
@@ -7,34 +7,46 @@ double pwm = 0.0;
 double Kp = 1.2;
 double Kd = 0.8;
 double e_prejsnji = 0.0, e = 0.0;
+const uint32_t interval = 1;
+uint32_t frameCount = 0;
 
-void setup() {
+void setup()
+{
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   Serial.begin(115200);
 }
 
-void loop() {
-  int setpoint = analogRead(A0);
-  long setpoint_m = map(setpoint, 0, 1023, 0, 255);
-  int osvetlitev = analogRead(A1);
-  long osvetlitev_m = map(osvetlitev, 0, 1023, 0, 255);
-  e = setpoint_m - osvetlitev_m;
-  pwm = Kp*e + Kd * (e-e_prejsnji);
-  if(pwm>255.0) pwm=255.0;
+void loop()
+{
+  uint32_t time = millis();
+  if ((millis() - time) > interval)
+  {
+    int setpoint = analogRead(A0);
+    long setpoint_m = map(setpoint, 0, 1023, 0, 255);
+    int osvetlitev = analogRead(A1);
+    long osvetlitev_m = map(osvetlitev, 0, 1023, 0, 255);
+    e = setpoint_m - osvetlitev_m;
+    pwm = Kp * e + Kd * (e - e_prejsnji)/(double)interval;
+    if (pwm > 255.0)
+      pwm = 255.0;
+      
+    Serial.print(frameCount++);
+    Serial.print("\t");
+    Serial.print(pwm);
+    Serial.print("\t");
+    Serial.print(setpoint_m);
+    Serial.print("\t");
+    Serial.println(osvetlitev_m);
 
-  Serial.print(pwm);
-  Serial.print(" i:");
-  Serial.print(setpoint_m);
-  Serial.print(" o:");
-  Serial.println(osvetlitev_m);
+    if (pwm < 0)
+      pwm = 0;
 
-  if(pwm < 0) pwm = 0;
-
-  analogWrite(led1,pwm);
-  analogWrite(led2,pwm);
-  analogWrite(led3,pwm);
-  delay(100);
-  e_prejsnji = e;
+    analogWrite(led1, pwm);
+    analogWrite(led2, pwm);
+    analogWrite(led3, pwm);
+    delay(100);
+    e_prejsnji = e;
+  }
 }
