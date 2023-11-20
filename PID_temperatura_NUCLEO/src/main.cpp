9673b5824ddc99f66639f6e77ff2317b2258b2c3
leonbl <leon.blazic@gmail.com>
2023-11-20 11:12:59 +0100

    Update main.cpp

--- PID_temperatura_NUCLEO/src/main.cpp
+++ PID_temperatura_NUCLEO/src/main.cpp
@@ -7,11 +7,12 @@
 HardwareTimer *Tim1;
 TIM_TypeDef *I1;
 uint32_t c1, pwm = 0;
+volatile bool computeNow = false;
 
-//Define Variables we'll be connecting to
+// Define Variables we'll be connecting to
 float Setpoint, Input, Output;
 float Kp = 2, Ki = 5, Kd = 1;
-//Specify PID links
+// Specify PID links
 QuickPID myPID(&Input, &Output, &Setpoint);
 
 void prekinitev_PID();
@@ -21,7 +22,7 @@ void setup()
   Serial.begin(115200);
   Input = analogRead(PIN_INPUT);
   Setpoint = 900;
-    //apply PID gains
+  // apply PID gains
   myPID.SetTunings(Kp, Ki, Kd);
 
   I1 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(PIN_OUTPUT), PinMap_PWM);
@@ -37,21 +38,26 @@ void setup()
   casovnik->attachInterrupt(prekinitev_PID);
   casovnik->resume();
 
-  //turn the PID on
+  // turn the PID on
   myPID.SetMode(myPID.Control::automatic);
 }
 
-void prekinitev_PID(){
-  Input = analogRead(PIN_INPUT);
-  myPID.Compute();
-  long pwm = map(Output, 0, 255, 0, 100);
-  Tim1->setCaptureCompare(c1, pwm, PERCENT_COMPARE_FORMAT);
-  Serial.print(Input);
-  Serial.print(" ");
-  Serial.println(pwm);
-
+void prekinitev_PID()
+{
+  computeNow = true;
 }
 
 void loop()
 {
+  if (computeNow)
+  {
+    Input = analogRead(PIN_INPUT);
+    myPID.Compute();
+    long pwm = map(Output, 0, 255, 0, 100);
+    Tim1->setCaptureCompare(c1, pwm, PERCENT_COMPARE_FORMAT);
+    Serial.print(Input);
+    Serial.print(" ");
+    Serial.println(pwm);
+    computeNow = false;
+  }
 }
